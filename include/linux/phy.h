6bfd0132d60e768bed17334c8c2b8dda28d0a352
Ioana Ciornei <ioana.ciornei@nxp.com>
2019-06-11 16:32:53 +0300

    net: phy: add interface mode PHY_INTERFACE_MODE_CAUI
    Add support for interface mode PHY_INTERFACE_MODE_CAUI.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -99,6 +99,7 @@ typedef enum {
 	/* 10GBASE-KR, XFI, SFI - single lane 10G Serdes */
 	PHY_INTERFACE_MODE_10GKR,
 	PHY_INTERFACE_MODE_USXGMII,
+	PHY_INTERFACE_MODE_CAUI,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -176,6 +177,8 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "10gbase-kr";
 	case PHY_INTERFACE_MODE_USXGMII:
 		return "usxgmii";
+	case PHY_INTERFACE_MODE_CAUI:
+		return "caui";
 	default:
 		return "unknown";
 	}
